dfb8d48605e51b2d262be55cf49cc628e721027f
Yaz1018 <garoncdavis@gmail.com>
2018-02-13 23:06:08 -0500

    repoed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+console.log('Hello Veronica');
+
+const csvFilePath='./Junk.csv'
+const csv=require('csvtojson')
+csv()
+.fromFile(csvFilePath)
+.on('json',(jsonObj)=>{
+    console.log(jsonObj)
+    // combine csv header row and csv line to a json object
+    // jsonObj.a ==> 1 or 4
+})
+.on('done',(error)=>{
+    console.log('File Processed! Thp Thp')
+})
